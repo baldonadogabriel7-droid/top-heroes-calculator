b2d2540b39396e8e2ffdcca89ce911779afdacde
baldonadogabriel7-droid <baldonadogabriel7@gmail.com>
2026-02-15 01:05:26 -1000

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+const buildingData = {
+    1: { wood: 100, stone: 80, gold: 20 },
+    2: { wood: 150, stone: 120, gold: 40 },
+    3: { wood: 220, stone: 180, gold: 60 },
+    4: { wood: 300, stone: 250, gold: 90 },
+    5: { wood: 400, stone: 350, gold: 120 }
+};
+
+const maxLevel = Object.keys(buildingData).length;
